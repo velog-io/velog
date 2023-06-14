3e42f3f1908fb6f18386ee64b15edf83b9252697
carrick <carrick@chafgames.com>
2023-06-14 20:22:55 +0900

    feat: Implements ipaddress from graph cdn

--- packages/velog-server/src/app.ts
+++ packages/velog-server/src/app.ts
@@ -1,7 +1,10 @@
 import Fastify from "fastify";
+import { ipaddr } from "@common/plugins/ipaddr.plugin";
 
 const app = Fastify({
   logger: true,
 });
+
+app.register(ipaddr);
 
 export default app;
---
+++ packages/velog-server/src/common/plugins/ipaddr.plugin.ts
@@ -0,0 +1,9 @@
+import type { FastifyPluginAsync } from "fastify";
+
+export const ipaddr: FastifyPluginAsync = async (fastify) => {
+  fastify.addHook("preHandler", async (request, reply, done) => {
+    const fromCdnIp = request.headers["gcdn-client-ip"];
+    const graphCdnAddress = Array.isArray(fromCdnIp) ? fromCdnIp[0] : fromCdnIp;
+    request.ipaddr = graphCdnAddress || request.ips?.slice(-1)[0] || request.ip;
+  });
+};
