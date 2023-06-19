f6c020693fa1791b7ebecd074cc2285c2409874e
carrick <carrick@chafgames.com>
2023-06-19 23:04:12 +0900

    fix: arrow function to named function

--- packages/velog-server/src/lib/utils/UtilsService.ts
+++ packages/velog-server/src/lib/utils/UtilsService.ts
@@ -4,10 +4,10 @@ import { injectable, singleton } from 'tsyringe'
 @injectable()
 @singleton()
 export class UtilsService {
-  public resolveDir = (dir: string): string => {
+  public resolveDir(dir: string): string {
     return resolve(process.cwd(), dir)
   }
-  public escapeForUrl = (text: string): string => {
+  public escapeForUrl(text: string): string {
     return text
       .replace(
         /[^0-9a-zA-Zㄱ-힣.\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf -]/g,
