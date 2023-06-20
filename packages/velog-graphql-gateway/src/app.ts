1dfee110a1627a79588ad333676c79c4afea5e1f
carrick <carrick@chafgames.com>
2023-06-20 15:49:35 +0900

    remove: grapqhl-gateway

--- packages/velog-graphql-gateway/src/app.ts
+++
@@ -1,8 +0,0 @@
-import Fastify from 'fastify'
-import { ENV } from 'src/env.js'
-
-const app = Fastify({
-  logger: true,
-})
-
-export default app
--- packages/velog-graphql-gateway/src/main.ts
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-import { ENV } from './env.js'
-
-function main() {
-  app.listen({ port: ENV.port })
-}
-
-main()
