211f8af2ba558a663c8a8eaf84f3d67114b179d3
winverse <public.winverse@gmail.com>
2023-06-21 12:03:34 +0900

    feat: apply svg loader with turbo

--- packages/velog-client/next.config.js
+++ packages/velog-client/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    turbo: {
+      rules: {
+        '.svg': ['@svgr/webpack'],
+      },
+    },
+  },
 }
 
 module.exports = nextConfig
