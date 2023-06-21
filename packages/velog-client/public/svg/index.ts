0ff21b0cee200ca8cca7fba3aace0011b7f982bc
winverse <public.winverse@gmail.com>
2023-06-21 10:28:46 +0900

    feat: init styles and add favicon

---
+++ packages/velog-client/public/svg/index.ts
@@ -0,0 +1,25 @@
+export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as FacebookIcon } from './icon-facebook.svg';
+export { ReactComponent as FacebookSquareIcon } from './icon-facebook-square.svg';
+export { ReactComponent as GoogleIcon } from './icon-google.svg';
+export { ReactComponent as GithubIcon } from './icon-github.svg';
+export { ReactComponent as GlobeIcon } from './icon-globe.svg';
+export { ReactComponent as LockIcon } from './icon-lock.svg';
+export { ReactComponent as AddListIcon } from './icon-add-list.svg';
+export { ReactComponent as ImageVector } from './vector-image.svg';
+export { ReactComponent as MinusBoxIcon } from './icon-minus-box.svg';
+export { ReactComponent as PlusBoxIcon } from './icon-plus-box.svg';
+export { ReactComponent as SeriesImage } from './image-series.svg';
+export { ReactComponent as LikeIcon } from './icon-like.svg';
+export { ReactComponent as ShareIcon } from './icon-share.svg';
+export { ReactComponent as ClipIcon } from './icon-clip.svg';
+export { ReactComponent as EmailIcon } from './icon-email.svg';
+export { ReactComponent as TwitterIcon } from './icon-twitter.svg';
+export { ReactComponent as ShareIcon2 } from './icon-share-2.svg';
+export { ReactComponent as SearchIcon } from './icon-search.svg'; // iconmonstr-magnifier-3
+export { ReactComponent as SearchIcon2 } from './icon-search-2.svg';
+export { ReactComponent as VelogIcon } from './velog-icon.svg';
+export { ReactComponent as CheckIcon } from './icon-check.svg';
+
+export { ReactComponent as MoonIcon } from './icon-moon.svg';
+export { ReactComponent as SunIcon } from './icon-sun.svg';
--- packages/velog-client/src/app/page.tsx
+++ packages/velog-client/src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main>
       <div>Velog</div>
     </main>
-  );
+  )
 }
