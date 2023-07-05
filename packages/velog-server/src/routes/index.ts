a17ede282606ad0aac99f46e0ca2b210756a8642
carrick <carrick@chafgames.com>
2023-07-05 23:01:09 +0900

    feat: setup create mock script

--- packages/velog-server/src/routes/index.ts
+++ packages/velog-server/src/routes/index.ts
@@ -4,6 +4,7 @@ const indexRoute: FastifyPluginAsync = async (fastify) => {
   fastify.get('/', async (request) => {
     const ip = request.ipaddr
     const user = request.user
+
     return { user, ip }
   })
 }
---
+++ packages/velog-server/test/mock/mockUser.ts
@@ -0,0 +1,33 @@
+export const mockUserWithProfile = [
+  {
+    username: 'velopert',
+    email: 'velopert@chafgames.com',
+    is_certified: true,
+    profile: {
+      display_name: 'velopert',
+      short_bio: 'CEO @ Chaf Inc. 사용자들이 좋아하는 프로덕트를 만듭니다.',
+      thumbnail:
+        'https://velog.velcdn.com/images/velopert/profile/ccab49cd-098c-4bb4-87dd-38044a403b61/17202261.png',
+    },
+  },
+  {
+    username: 'carrick',
+    email: 'carrick@chafgames.com',
+    is_certified: true,
+    profile: {
+      display_name: 'carrick',
+      short_bio: 'TypeScript developer',
+      thumbnail: null,
+    },
+  },
+  {
+    username: 'riverKey',
+    email: 'river.key93@gmail.com',
+    is_certified: true,
+    profile: {
+      display_name: 'key',
+      short_bio: 'Frontend Developer',
+      thumbnail: null,
+    },
+  },
+]
