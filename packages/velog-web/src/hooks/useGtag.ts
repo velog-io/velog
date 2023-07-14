7f5c2e72183779250e27ef1cd39250840b50ff31
carrick <carrick@chafgames.com>
2023-07-15 01:59:39 +0900

    feat: implement gtag hooks

---
+++ packages/velog-web/src/hooks/useGtag.ts
@@ -0,0 +1,23 @@
+type EventAction =
+  | 'recommend_click'
+  | 'recommend_guest_click'
+  | 'ads_click'
+  | Gtag.EventNames
+
+type EventOptions =
+  | {
+      userId?: string
+    }
+  | Gtag.ControlParams
+  | Gtag.EventParams
+  | Gtag.CustomParams
+
+export default function useGtag() {
+  function gtag(action: EventAction, eventOptions: EventOptions) {
+    if (!window.gtag) return
+
+    window.gtag('event', action, eventOptions)
+  }
+
+  return { gtag }
+}
