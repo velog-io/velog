8875943b05a670004a2a188a899a535c0a6f5fc6
carrick <carrick@chafgames.com>
2023-07-18 12:50:09 +0900

    feat:  seperate docker compose file

--- packages/velog-server/src/lib/db/DbService.ts
+++ packages/velog-server/src/lib/db/DbService.ts
@@ -1,16 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { injectable, singleton } from 'tsyringe'
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
 @injectable()
 @singleton()
-export class DbService extends PrismaClient {
-  constructor() {
-    super()
-  }
-  async $connect() {
-    console.log('sleep...')
-    await sleep(3000)
-  }
-}
+export class DbService extends PrismaClient {}
