49c4629800ca0fec8e9514539a237ce32fc55a08
carrick <carrick@chafgames.com>
2023-07-20 13:48:31 +0900

    chore: remove console

--- infrastructure/lib/prefix.ts
+++ infrastructure/lib/prefix.ts
@@ -1,7 +1,7 @@
 import * as pulumi from '@pulumi/pulumi'
 
 const config = new pulumi.Config()
-console.log('test')
+
 const appEnv = config.require('APP_ENV')
 
 export const prefix = `${config.name}-${appEnv}`
