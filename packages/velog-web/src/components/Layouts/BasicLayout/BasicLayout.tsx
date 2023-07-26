3353613ce806862c5adcd32da657784f5df93c06
carrick <carrick@chafgames.com>
2023-07-26 17:17:52 +0900

    feat: to static file from trending posts page

--- packages/velog-web/src/components/Layouts/BasicLayout/BasicLayout.tsx
+++ packages/velog-web/src/components/Layouts/BasicLayout/BasicLayout.tsx
@@ -2,6 +2,7 @@ import { bindClassNames } from '@/lib/styles/bindClassNames'
 import styles from './BasicLayout.module.css'
 import Header from '@/components/Header/Header'
 import HomeTab from '@/features/home/components/HomeTab/HomeTab'
+import { Suspense } from 'react'
 
 const cx = bindClassNames(styles)
 
@@ -13,7 +14,9 @@ async function BasicLayout({ children }: Props) {
   return (
     <div className={cx('block')}>
       <Header />
-      <HomeTab />
+      <Suspense>
+        <HomeTab />
+      </Suspense>
       <div className={cx('mainWrapper')}>
         <main>{children}</main>
       </div>
