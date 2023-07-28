de00931d896ab9f4fae4af7dcd8375401b73d238
carrick <carrick@chafgames.com>
2023-07-28 16:02:42 +0900

    chore: restore scroll

--- packages/velog-web/src/features/home/hooks/useRecentPosts.tsx
+++ packages/velog-web/src/features/home/hooks/useRecentPosts.tsx
@@ -6,8 +6,8 @@ import {
   RecentPostsQueryVariables,
 } from '@/graphql/generated'
 import { Posts } from '@/types/post'
-import { useInfiniteQuery } from '@tanstack/react-query'
-import { useMemo, useRef } from 'react'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useEffect, useMemo, useRef } from 'react'
 
 export default function useRecentPosts(initialPosts: Posts[] = []) {
   const hasCheckedRef = useRef<boolean>(false)
@@ -21,28 +21,54 @@ export default function useRecentPosts(initialPosts: Posts[] = []) {
     }
   }, [initialPosts, limit])
 
-  const { data, fetchNextPage, isFetching, hasNextPage } = useInfiniteQuery<RecentPostsQuery>(
-    ['recentPosts', { input: fetchInput }],
-    ({ pageParam = fetchInput }) =>
-      fetcher<RecentPostsQuery, RecentPostsQueryVariables>(RecentPostsDocument, {
-        input: pageParam,
-      })(),
-    {
-      retryDelay: 100,
-      cacheTime: 1000 * 60 * 1,
-      staleTime: 1000 * 60 * 1,
-      enabled: hasCheckedRef.current,
-      getNextPageParam: (page) => {
-        const recentPosts = page.recentPosts
-        if (!recentPosts) return false
-        if (recentPosts && recentPosts?.length < limit) return false
-        return {
-          limit,
-          cursor: recentPosts[recentPosts.length - 1]?.id,
-        }
-      },
-    }
-  )
+  const { data, isLoading, fetchNextPage, isFetching, hasNextPage } =
+    useInfiniteQuery<RecentPostsQuery>(
+      ['recentPosts', { input: fetchInput }],
+      ({ pageParam = fetchInput }) =>
+        fetcher<RecentPostsQuery, RecentPostsQueryVariables>(RecentPostsDocument, {
+          input: pageParam,
+        })(),
+      {
+        retryDelay: 100,
+        cacheTime: 1000 * 60 * 1,
+        staleTime: 1000 * 60 * 1,
+        enabled: hasCheckedRef.current,
+        getNextPageParam: (page) => {
+          const recentPosts = page.recentPosts
+          if (!recentPosts) return false
+          if (recentPosts && recentPosts?.length < limit) return false
+          return {
+            limit,
+            cursor: recentPosts[recentPosts.length - 1]?.id,
+          }
+        },
+      }
+    )
+
+  // TODO: remove Start
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    if (hasCheckedRef.current) return
+    hasCheckedRef.current = true
+
+    try {
+      const stringPosts = localStorage.getItem('recentPosts')
+      if (!stringPosts) return
+      const parsed = JSON.parse(stringPosts)
+      queryClient.setQueriesData(['recentPosts', { input: fetchInput }], parsed)
+    } catch (_) {}
+  }, [queryClient, fetchInput])
+
+  useEffect(() => {
+    const scrolly = Number(localStorage.getItem('scrollPosition'))
+    if (!scrolly || isLoading) return
+    window.scrollTo({
+      top: Number(scrolly),
+    })
+    localStorage.removeItem('recentPosts')
+    localStorage.removeItem('scrollPosition')
+  }, [isLoading])
+  // TODO: remove End
 
   const posts = useMemo(() => {
     return [...initialPosts, ...(data?.pages?.flatMap((page) => page.recentPosts) || [])] as Posts[]
