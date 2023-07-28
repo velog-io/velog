8d2facada1d71453fe0524d82600672e68147838
carrick <carrick@chafgames.com>
2023-07-28 16:46:23 +0900

    chore: clean server code

--- packages/velog-server/src/services/CommentService/CommentServiceInterface.ts
+++
@@ -1,3 +0,0 @@
-export interface CommentServiceInterface {
-  count(postId: string): Promise<number>
-}
--- packages/velog-server/src/services/PostService/PostServiceInterface.ts
+++ packages/velog-server/src/services/PostService/PostServiceInterface.ts
@@ -1,25 +1,4 @@
-import {
-  ReadPostInput,
-  ReadingListInput,
-  RecentPostsInput,
-  TrendingPostsInput,
-} from '@graphql/generated'
-import { Post, Prisma } from '@prisma/client'
-
-export interface PostServiceInterface {
-  getReadingList(input: ReadingListInput, userId: string | undefined): Promise<Post[]>
-  // private getPostsByRead(input: GetPostsByTypeParams): Promise<Post[]>
-  // private getPostsByLiked(input: GetPostsByTypeParams): Promise<Post[]>
-  getRecentPosts(input: RecentPostsInput, userId: string | undefined): Promise<Post[]>
-  getTrendingPosts(input: TrendingPostsInput, ip: string | null): Promise<Post[]>
-  getPost(input: ReadPostInput, userId: string | undefined): Promise<Post | null>
-}
-
-export type GetPostsByTypeParams = {
-  cursor: string | undefined
-  userId: string
-  limit: number
-}
+import { Prisma } from '@prisma/client'
 
 export type PostAllInclude = Prisma.PostGetPayload<{
   include: {
@@ -43,3 +22,9 @@ export type PostAllInclude = Prisma.PostGetPayload<{
     comment?: true
   }
 }>
+
+export type GetPostsByTypeParams = {
+  cursor: string | undefined
+  userId: string
+  limit: number
+}
