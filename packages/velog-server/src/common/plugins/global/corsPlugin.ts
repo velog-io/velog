0bd443a52f46fcf1f040b2777db145c516f9ea80
carrick <carrick@chafgames.com>
2023-08-18 01:12:46 +0900

    chore: add cors whitelist

--- packages/velog-server/src/common/plugins/global/corsPlugin.ts
+++ packages/velog-server/src/common/plugins/global/corsPlugin.ts
@@ -14,6 +14,7 @@ const corsPlugin: FastifyPluginAsync = async (fastify) => {
 
   if (ENV.appEnv === 'development') {
     corsWhitelist.push(/^http:\/\/localhost/)
+    corsWhitelist.push(/^https:\/\/velog.pro$/)
   }
 
   fastify.register(cors, {
