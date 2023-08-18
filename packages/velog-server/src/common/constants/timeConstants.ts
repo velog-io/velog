0fa1e0e0d6a80f23270824267ad5369e9dbf47a8
carrick <carrick@chafgames.com>
2023-08-18 17:30:33 +0900

    chore: remove time constants

--- packages/velog-server/src/common/constants/timeConstants.ts
+++
@@ -1,7 +0,0 @@
-export const ONE_MINUTE_IN_MS = 1000 * 60
-export const ONE_HOUR_IN_MS = 60 * ONE_MINUTE_IN_MS
-export const ONE_DAY_IN_MS = 24 * ONE_HOUR_IN_MS
-
-export const ONE_MINUTE_S = 60
-export const ONE_HOUR_S = 60 * ONE_MINUTE_S
-export const ONE_DAY_S = 24 * ONE_HOUR_S
