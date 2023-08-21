ccec3d7ba070477545f3c7df252da881067c36a0
carrick <carrick@chafgames.com>
2023-08-21 16:22:56 +0900

    feat: add db connected message in cron

--- packages/velog-cron/src/main.ts
+++ packages/velog-cron/src/main.ts
@@ -3,10 +3,16 @@ import 'reflect-metadata'
 import { ENV } from '@env'
 import { disableKeepAlive } from '@plugins/encapsulated/closePlugin.js'
 import app from './app.js'
+import { container } from 'tsyringe'
+import { DbService } from '@lib/db/DbService.js'
 
 async function main() {
   app.listen({ port: ENV.port })
+  const dbService = container.resolve(DbService)
 
+  await dbService.$connect()
+
+  console.info(`INFO: Database connected to "${ENV.databaseUrl}"`)
   process.on('SIGINT', async () => {
     disableKeepAlive()
     process.exit(0)
