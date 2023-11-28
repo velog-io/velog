5ac66e0df57eff6900dc4b14a7504c23045618a9
carrick <carrick@chafgames.com>
2023-11-29 01:18:19 +0900

    chore: remove unused header component

--- packages/velog-web/src/components/HeaderCustomLogo/HeaderCustomLogo.test.tsx
+++
@@ -1,8 +0,0 @@
-import HeaderCustomLogo from './HeaderCustomLogo'
-import { render } from '@testing-library/react'
-
-describe('HeaderCustomLogo', () => {
-  it('renders successfully', () => {
-    render(<HeaderCustomLogo />)
-  })
-})
