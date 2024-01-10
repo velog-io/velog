67718690f011e028e9a5f66262f526ca7a210f1b
carrick <carrick@chafgames.com>
2024-01-10 13:38:25 +0900

    fix: notification schema

--- packages/velog-prisma/prisma/migrations/20240110043223_create_notification_table/migration.sql
+++ packages/velog-prisma/prisma/migrations/20240110043223_create_notification_table/migration.sql
@@ -3,11 +3,13 @@ CREATE TABLE "notifications" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "type" VARCHAR(50) NOT NULL,
     "message" VARCHAR(512) NOT NULL,
+    "link" VARCHAR(512) NOT NULL,
     "fk_user_id" UUID NOT NULL,
     "action" JSONB NOT NULL DEFAULT '{}',
     "action_id" UUID NOT NULL,
     "is_read" BOOLEAN NOT NULL DEFAULT false,
     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "read_at" TIMESTAMP(3),
     "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
