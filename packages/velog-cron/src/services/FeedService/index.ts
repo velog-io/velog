a3dfd697289155d94f4095761d8b31f7989d94e3
carrick <carrick@chafgames.com>
2024-03-25 15:25:30 +0900

    fix: error handling in GenerateFeedJob and correct variable name in FeedService

--- packages/velog-cron/src/services/FeedService/index.ts
+++ packages/velog-cron/src/services/FeedService/index.ts
@@ -20,11 +20,11 @@ export class FeedService implements Service {
   public async createFeed({ followingId, postId }: CreateFeedArgs): Promise<void> {
     const followers = await this.followUserService.getFollowers(followingId)
     const followerIds = followers.map((user) => user.id)
-    for (const userId of followerIds) {
+    for (const followeId of followerIds) {
       try {
         await this.db.feed.create({
           data: {
-            fk_user_id: userId,
+            fk_user_id: followeId,
             fk_post_id: postId,
           },
         })
