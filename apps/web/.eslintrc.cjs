6daa2f929ae840b85637b36028000308aed4ecf0
carrick <carrick@chafgames.com>
2024-04-04 15:25:10 +0900

    refactor: apply to lint

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,11 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'next/core-web-vitals',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
+    'prettier/prettier': ['error', { semi: false, singleQuote: true }],
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
