5eed57a4c1b8aba2a0c2de3af4b2cfe61df8d73d
carrick <carrick@chafgames.com>
2024-04-08 15:30:43 +0900

    chore: add cache for lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-  ignorePatterns: ['apps/**', 'packages/**', 'node_modules/', 'dist/'],
+  ignorePatterns: ["apps/**", "packages/**", "node_modules/", "dist/"],
   env: {
     node: true,
     jest: true,
   },
-}
+  globals: {
+    react: true,
+    jsx: true,
+  },
+};
