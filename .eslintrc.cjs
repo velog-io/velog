bd8bab4b5c5a2298bfeaeb8cbf4ced13853f5e77
carrick <carrick@chafgames.com>
2024-04-08 15:16:22 +0900

    fix: lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,12 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
+    project: true,
   },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  root: true,
+  ignorePatterns: ['apps/**', 'packages/**', 'node_modules/', 'dist/'],
   env: {
     node: true,
     jest: true,
-  },
-  rules: {
-    'prettier/prettier': ['error', { semi: false, singleQuote: true }],
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
   },
 }
