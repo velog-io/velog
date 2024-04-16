351caa1e158ad6fbd0fd248c7f23768eabeb9d52
carrick <carrick@chafgames.com>
2024-04-16 21:22:18 +0900

    fix: get access_token error

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -3,8 +3,9 @@
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 import { cookies } from 'next/headers'
 
-export const getAccessToken = (): RequestCookie | undefined => {
+export const getAccessToken = async (): Promise<RequestCookie | undefined> => {
   const cookieStore = cookies()
+
   const cookie = cookieStore.get('access_token') || cookieStore.get('refresh_token')
   return cookie
 }
