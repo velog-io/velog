a62da329bcce25852a076ec7b99cbdf8d2af567f
carrick <carrick@chafgames.com>
2024-04-16 20:28:51 +0900

    feat: update packages in web

--- apps/web/docker-compose.sh
+++ apps/web/docker-compose.sh
@@ -12,11 +12,11 @@ export TURBO_TEAM=$(aws configure get turbo_team --profile $AWS_PROFILE)
 export TURBO_TOKEN=$(aws configure get turbo_token --profile $AWS_PROFILE)
 export TURBO_REMOTE_CACHE_SIGNATURE_KEY=$(aws configure get turbo_remote_cache_signature_key --profile $AWS_PROFILE)
 
-echo "AWS 프로필 설정이 완료되었습니다. $AWS_PROFILE 프로필을 사용합니다."
-echo "AWS_ACCESSKEY_ID: $AWS_ACCESS_KEY_ID"
-echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
-echo "TURBO_TEAM: $TURBO_TEAM"
-echo "TURBO_TOKEN: $TURBO_TOKEN"
-echo "TURBO_REMOTE_CACHE_SIGNATURE_KEY: $TURBO_REMOTE_CACHE_SIGNATURE_KEY"
+# echo "AWS 프로필 설정이 완료되었습니다. $AWS_PROFILE 프로필을 사용합니다."
+# echo "AWS_ACCESSKEY_ID: $AWS_ACCESS_KEY_ID"
+# echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
+# echo "TURBO_TEAM: $TURBO_TEAM"
+# echo "TURBO_TOKEN: $TURBO_TOKEN"
+# echo "TURBO_REMOTE_CACHE_SIGNATURE_KEY: $TURBO_REMOTE_CACHE_SIGNATURE_KEY"
 
 docker-compose up --build
